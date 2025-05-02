41caf1129de4d008063b331e01d3813f97c3e520
Emem Edem Junior <106032953+JuniorEmem6@users.noreply.github.com>
2025-05-02 16:59:35 +0100

    Update app.js
    New fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-const users = [];
+const users = []
 
 app.get("/users", (req, res) => {
   return res.send(users);
